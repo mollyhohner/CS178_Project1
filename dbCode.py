8032ec0e1fe02eccf1921ebbba2836e947e57c0e
mollyhohner <molly.hohner@drake.edu>
2025-04-09 22:21:01 -0500

    adding stuff

--- dbCode.py
+++ dbCode.py
@@ -1,7 +1,23 @@
-#import pymysql
-#import pymysql.cursors
+import pymysql
+import pymysql.cursors
 import creds
 import boto3
 
 #Lab 13
 # Section 1: MySQL
+
+def get_conn():
+    conn = pymysql.connect(
+        host= creds.host,
+        user= creds.user,
+        password = creds.password,
+        db=creds.db,
+    )
+    return conn
+
+def execute_query(query, args=()):
+    cur = get_conn.cursor(pymysql.cursors.DictCursor)
+    cur.execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
---
+++ flaskapp.py
@@ -0,0 +1,71 @@
+from flask import Flask
+from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello from Flask!</h1>'
+
+'''
+app = Flask(__name__)
+app.secret_key = 'your_secret_key' # this is an artifact for using flash displays; 
+                                   # it is required, but you can leave this alone
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/add-user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        # Extract form data
+        first_name = request.form['first name']
+        last_name = request.form['last name']
+        genre = request.form['genre']
+        
+        # Process the data (e.g., add it to a database)
+        # For now, let's just print it to the console
+        print("First Name:", first_name, ":", "Last Name:", last_name, ":", "Favorite Genre:", genre)
+        
+        flash('User added successfully!', 'success')  # 'success' is a category; makes a green banner at the top
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('add_user.html')
+
+
+@app.route('/display-users')
+def display_users():
+    # hard code a value to the users_list;
+    # note that this could have been a result from an SQL query :) 
+    users_list = (('John','Doe','Comedy'),('Jane', 'Doe','Drama'))
+    return render_template('display_users.html', users = users_list)
+
+
+@app.route('/delete-user', methods=['GET', 'POST'])
+def delete_user():
+    if request.method == 'POST':
+        # Extract form data
+        name = request.form['name']
+
+        
+        # Process the data (e.g., add it to a database)
+        # For now, let's just print it to the console
+        print("Name:", name)
+        
+        flash('User deleted successfully!', 'warning')  # options include: success, error, warning, info
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('delete_user.html')
+
+
+'''
+# these two lines of code should always be the last in the file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
